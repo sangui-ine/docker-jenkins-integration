923f944c48c0b5f800713520140bff9a237e3544
sonal <sonaliinindiaa@gmail.com>
2022-07-05 12:00:31 +0530

    initial commit

---
+++ src/test/java/jenkinsdocker/jenkinsdocker/JenkinsdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkinsdocker.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
